9955fb0353f914b11fdf81434d4a9c612f9c50d4
yamkelajojo <mhlengijojo7@gmail.com>
2023-08-28 21:53:57 +0200

    just tried to add Header component so another Guyb on FB can try and fix it

---
+++ components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header id="biggerHeader">
+    <header id="smallerNavHeader">
+      <nav class="navbar">
+        <div class="logoDiv">
+          <NuxtLink id="title" to="/">
+            <h1>ThaSto'</h1>
+          </NuxtLink>
+        </div>
+        <button class="hamburgerButton" @click="toggleSideMenu">
+          <Icon name="nimbus:align-left" size="1.5rem" />
+        </button>
+        <div id="navTabsDiv">
+          <NuxtLink to="/"><p>Home</p></NuxtLink>
+          <NuxtLink to="/products"><p>Products</p></NuxtLink>
+          <NuxtLink to="/about"><p>About</p></NuxtLink>
+        </div>
+      </nav>
+    </header>
+  </header>
+</template>
+
+<script setup>
+const emit = defineEmits(["toggleSM"]);
+
+const toggleSideMenu = () => {
+  console.log("here");
+  emit("toggleSM");
+};
+</script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,6 @@
 <template>
   <div id="layerBody">
-    <header id="biggerHeader">
-      <header id="smallerNavHeader">
-        <nav class="navbar">
-          <div class="logoDiv">
-            <NuxtLink id="title" to="/">
-              <h1>ThaSto'</h1>
-            </NuxtLink>
-          </div>
-          <button class="hamburgerButton" @click="toggleSideMenu">
-            <Icon name="nimbus:align-left" size="1.5rem" />
-          </button>
-          <div id="navTabsDiv">
-            <NuxtLink to="/"><p>Home</p></NuxtLink>
-            <NuxtLink to="/products"><p>Products</p></NuxtLink>
-            <NuxtLink to="/about"><p>About</p></NuxtLink>
-          </div>
-        </nav>
-      </header>
-    </header>
-
+    <Header :toggleSM="toggleSideMenu"></Header>
     <transition name="overlay">
       <div class="overlay" v-if="sideNavShown" @click="toggleSideMenu"></div>
     </transition>
